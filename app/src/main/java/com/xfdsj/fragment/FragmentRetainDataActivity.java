cd3c37954f2ff2b94e7114fd3d2496133412ecb2
Sting926GitHub <sting_926@163.com>
2016-03-06 14:30:50 +0800

    update

---
+++ app/src/main/java/com/xfdsj/fragment/FragmentRetainDataActivity.java
@@ -0,0 +1,13 @@
+package com.xfdsj.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentRetainDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_fragment_retain_data);
+    }
+}
