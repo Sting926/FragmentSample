3a88fc683ea4f0d06cd3128377fd8ac7fda4e570
jian <sj926@sina.com>
2016-03-07 18:20:48 +0800

    update

--- app/src/main/java/com/xfdsj/fragment/FragmentRetainDataActivity.java
+++ app/src/main/java/com/xfdsj/fragment/FragmentRetainDataActivity.java
@@ -1,21 +1,13 @@
 package com.xfdsj.fragment;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import java.io.IOException;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
-public class FragmentRetainDataActivity extends Activity {
+public class FragmentRetainDataActivity extends Activity implements RetainedFragment.LoadComplete {
 
   private RetainedFragment retainedFragment;
   private ProgressDialogFragment progressDialog;
@@ -27,51 +19,36 @@ public class FragmentRetainDataActivity extends Activity {
     ButterKnife.bind(this);
     // find the retained fragment on activity restarts
     retainedFragment = (RetainedFragment) getFragmentManager().findFragmentByTag("data");
+    progressDialog = (ProgressDialogFragment) getFragmentManager().findFragmentByTag("dialog");
 
     // create the fragment and data the first time
     if (retainedFragment == null) {
       // add the fragment
       retainedFragment = new RetainedFragment();
       getFragmentManager().beginTransaction().add(retainedFragment, "data").commit();
-    }else {
-      ivPic.setImageBitmap(retainedFragment.getPic());
+    } else {
+      ivPic.setImageBitmap(retainedFragment.getPic());//注掉这行后横竖屏切换后图片消失
     }
   }
 
   @OnClick(R.id.iv_pic) void picClick() {
-    if (ivPic.getDrawable() != null)
-      return;
+    if (ivPic.getDrawable() != null) return;
     progressDialog = new ProgressDialogFragment();
     progressDialog.show(getFragmentManager(), "dialog");
-    OkHttpClient client = new OkHttpClient();
-    Request request =
-        new Request.Builder().url("http://img.blog.csdn.net/20160307163830263")
-            .build();
-    client.newCall(request).enqueue(new Callback() {
-      @Override public void onFailure(Call call, IOException e) {
-
-      }
-
-      @Override public void onResponse(Call call, Response response) throws IOException {
-        final Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
-        try {
-          Thread.sleep(3000);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        runOnUiThread(new Runnable() {
-          @Override public void run() {
-            ivPic.setImageBitmap(bitmap);
-            retainedFragment.setPic(bitmap);
-            progressDialog.dismiss();
-          }
-        });
-      }
-    });
+    retainedFragment.loadPic("http://img.blog.csdn.net/20160307170349803");
   }
 
   @Override protected void onDestroy() {
     super.onDestroy();
     ButterKnife.unbind(this);
+  }
+
+  @Override public void onComplete() {
+    runOnUiThread(new Runnable() {
+      @Override public void run() {
+        ivPic.setImageBitmap(retainedFragment.getPic());
+        progressDialog.dismiss();
+      }
+    });
   }
 }
--- app/src/main/java/com/xfdsj/fragment/RetainedFragment.java
+++ app/src/main/java/com/xfdsj/fragment/RetainedFragment.java
@@ -1,8 +1,15 @@
 package com.xfdsj.fragment;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.graphics.Bitmap;
-import android.os.Bundle;
+import android.graphics.BitmapFactory;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -11,16 +18,52 @@ public class RetainedFragment extends BaseFragment {
 
   private Bitmap mBitmap;
 
+  private LoadComplete loadComplete;
+
+  public interface LoadComplete {
+    public void onComplete();
+  }
+
   public RetainedFragment() {
     // Required empty public constructor
     setRetainInstance(true);
   }
 
-  public void setPic(Bitmap bitmap){
-    mBitmap = bitmap;
+  @Override public void onAttach(Activity activity) {
+    super.onAttach(activity);
+    try {
+      if (activity instanceof LoadComplete) {
+        loadComplete = (LoadComplete) activity;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
-  public Bitmap getPic(){
+  public void loadPic(String url) {
+    OkHttpClient client = new OkHttpClient();
+    Request request = new Request.Builder().url(url).build();
+    client.newCall(request).enqueue(new Callback() {
+      @Override public void onFailure(Call call, IOException e) {
+
+      }
+
+      @Override public void onResponse(Call call, Response response) throws IOException {
+        final Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+        try {
+          Thread.sleep(3000);
+          mBitmap = bitmap;
+          if (loadComplete != null) {
+            loadComplete.onComplete();
+          }
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+
+  public Bitmap getPic() {
     return mBitmap;
   }
 }
